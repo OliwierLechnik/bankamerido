3d9851b975999cf263df4799f4477c1128047c15
tokit3mx <43906321+tokit3mx@users.noreply.github.com>
2023-08-07 01:23:19 +0200

    wstepne przygotowania do wprowadzenia kredytu

--- account/models.py
+++ account/models.py
@@ -5,6 +5,7 @@ class Account(models.Model):
     owner = models.CharField(max_length=127)
     name = models.CharField(max_length=127)
     balance = models.IntegerField(default=0)
+    credit = models.IntegerField(default=0)
     active = models.BooleanField(default=False)
     super = models.BooleanField(default=False)
 
